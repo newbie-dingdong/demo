897ce9359ab9576a8c1a43fe45b5417aa9f11860
范才桂 <2470530249@qq.com>
2023-06-05 13:42:58 +0800

    fix: cz

--- .cz-config.js
+++ .cz-config.js
@@ -8,57 +8,59 @@ module.exports = {
       name: "✨ feat:      新功能"
     },
     {
-      value: "fix: ",
+      value: "fix",
       name: "🐛 fix:      修复"
     },
     {
-      value: "docs: ",
+      value: "docs",
       name: "📝 docs:      文档变更"
     },
     {
-      value: "style: ",
+      value: "style",
 
       name: "💄 style:     代码格式(不影响代码运行的变动)"
     },
     {
-      value: "refactor: ",
+      value: "refactor",
 
       name: "♻️  refactor:    重构 (既不增加feature, 也不是修复bug)"
     },
     {
-      value: "perf: ",
+      value: "perf",
       name: "⚡️ perf:      性能优化"
     },
     {
-      value: "test: ",
+      value: "test",
       name: "✅ test:      增加测试"
     },
     {
-      value: "chore: ",
+      value: "chore",
       name: "🔧 chore:     构建过程或辅助工具的变动"
     },
     {
-      value: "revert: ",
+      value: "revert",
       name: "⏪ revert:     回退"
     },
     {
-      value: "build: ",
+      value: "build",
       name: "🚀 build:     打包"
     }
   ],
-
+  scopes: [{ name: "accounts" }, { name: "admin" }, { name: "exampleScope" }, { name: "changeMe" }],
+  allowTicketNumber: false,
+  isTicketNumberRequired: false,
   // 步骤
-  allowCustomScopes: true,
-  skipQuestions:['body','footer'],
   messages: {
     type: "请选择提交的类型：",
-    scope: "请选择一个scope(可选)：",
-    customScope: "情输入修改的范围(可选)",
+    scope: "选择一个scope(可选)",
+    customScope: "请输入修改的范围(可选)",
     subject: "请简要描述提交(必填)",
     body: "请输入详细描述(可选)",
     footer: "请输入要关闭的issus(可选)",
     confirmCommit: "确认要使用以上信息提交？(y/n)"
   },
+  allowCustomScopes: true,
+  skipQuestions: ["body", "footer"],
   // 默认长度72
-  subjectLimit: 72
+  subjectLimit: 100
 };
